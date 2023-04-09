f840ce92694afab1c79c62a1fd24490070c3fec6
FetusCoder <127114506+FetusCoder@users.noreply.github.com>
2023-04-09 16:22:12 +0700

    Updates all written in readme

--- README.md
+++ README.md
@@ -7,4 +7,13 @@ etch-a-sketch
 5. Added event listener that triggers on mouseover for above function at the end of the loop
 6. Created an adjustBtn inside .main div
 7. Initialize container width and height as variables in js file
-8. Initialize  
+8. Initialize gridSize of divElement created by loop
+9. At the start, i put a fix width and height to the divElement in css file (16px);
+10. I wanted to adjust the gridSize using the adjustBtn (probably going too far here because i got lost)
+11. Asked chatGPT and it helped me with certain parts of createGrid() and adjustBtn
+12. The box sketch pad only worked at 16px and left blank spaces to the right due to flex-wrap
+13. Used flex-basis: calc(100% / columns) and initialize rows and columns as global in js
+14. Used flex-grow: 1 to stretch each divElement evenly to fill up blank spaces
+15. Used height: 10% to try and hide the last row which will have insufficient divs and stretch to fill blank space(last row is still scuffed which is understandable because this is bandaid)
+16. Modify changeColor function from only applying black to random rgb colors
+17. Main takeaways are using a for loop to create divs and using the e(event object) to target the divs
--- script.js
+++ script.js
@@ -1,32 +1,61 @@
 const container = document.querySelector('.gridContainer');
 
 const containerSize = 500;
-const gridSize = 16;
+let gridSize = 16;
 
-const rows = Math.floor(containerSize / gridSize);
-const columns = Math.floor(containerSize / gridSize);
+let rows, columns;
 
-//Loop through container and create divs
-for (let i = 0; i < rows * columns; i++){
-    const divElement = document.createElement('div');
-    divElement.classList.add('gridElement')
-    container.appendChild(divElement);
-    divElement.addEventListener('mouseover', changeColor);
-}
+//function changeSize(newSize) {
+//    gridSize = newSize;
+//}
+
+function createGrid() {
+    // Clear existing grid
+    container.innerHTML = '';
+  
+    // Calculate rows and columns based on gridSize
+    const rows = containerSize / gridSize;
+    const columns = containerSize / gridSize;
+
+    container.style.width = `${rows * gridSize}`;
+    container.style.height = `${columns * gridSize}`;
+  
+    // Loop through container and create divs
+    for (let i = 0; i < rows * columns; i++) {
+      const divElement = document.createElement('div');
+      divElement.classList.add('gridElement');
+      divElement.style.width = `${gridSize}px`;
+      divElement.style.height = `${gridSize}px`;
+      container.appendChild(divElement);
+      divElement.addEventListener('mouseover', changeColor);
+    }
+  }
 
 //Target gridElement property and set css hover style
 function changeColor(e) {
     const gridElement = e.target;
-    gridElement.setAttribute("style", "background-color:black;");
+    const redColor = Math.floor(Math.random() * 256);
+    const greenColor = Math.floor(Math.random() * 256);
+    const blueColor = Math.floor(Math.random() * 256);
+    const randomColor = `rgb(${redColor}, ${greenColor} , ${blueColor})`;
+    gridElement.style.backgroundColor = randomColor;
 }
 
 const adjustBtn = document.querySelector('#adjustBtn')
 
 adjustBtn.addEventListener('click', () => {
-    let adjustSize = prompt("Type the size of pixels you want each div to be")
-    console.log(adjustSize);
-})
-
-function changeSize() {
-
-}
+    // Prompt user for new gridSize
+    const newGridSize = prompt("Type the number of rows and columns(1 to 100)");
+    while (newGridSize === null || isNaN(newGridSize) || newGridSize < 1 || newGridSize > 100) {
+        newGridSize = prompt("Type the number of rows and columns (1 to 100):");
+        if (newGridSize === null) {
+            return; // exit if user cancels prompt
+        }
+        newGridSize = parseInt(newGridSize);
+    }
+    gridSize = newGridSize;
+    createGrid();
+});
+  
+  // Create initial grid on window load
+  window.onload = () => createGrid();
